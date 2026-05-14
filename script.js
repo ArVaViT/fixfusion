ea744d45d866a051c129782c04a8d9807f85f79e
Kushal B G <kushalbg06@gmail.com>
2026-05-14 23:01:50 +0530

    add scroll-to-top button

--- script.js
+++ script.js
@@ -574,3 +574,20 @@
   }
 
 })();
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
